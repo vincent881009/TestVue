475aa735071958635e42a69fbd0f2fd1e81030a0
Akrios yu <gacktsong@hotmail.com>
2020-09-18 00:52:52 +0800

    fix

--- vue-admin/src/router/index.js
+++ vue-admin/src/router/index.js
@@ -10,7 +10,7 @@ const routes = [
   {
     path: "/Login",
     name: "Login",
-    component: import("../views/Login/Login.vue")
+    component: import("../views/Login/Index.vue")
   },
   {
     path: "/about",
---
+++ vue-admin/src/views/Login/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login" class="login">
+    登录页
+    <el-button disabled>默认按钮</el-button>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  background-color: lightblue;
+}
+</style>
